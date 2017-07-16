f11b170935f33bd876f719ebfad39a820e2b85a2
Saurabh Totey <SaurabhTotey@gmail.com>
2017-07-15 19:39:03 -0600

    Made (ideally) complete ChallengeBot class

--- challengebot.js
+++ challengebot.js
@@ -5,6 +5,7 @@ class ChallengeBot{
         this.isConnected = false;
         this.challenges = {};
         this.currentChallenge = "";
+        this.lastTimeChallenging = null;
 
         var RtmClient = require('@slack/client').RtmClient;
         var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
@@ -34,13 +35,40 @@ class ChallengeBot{
             }
             this.challenges[args[3]] = args.splice(4, args.length).join(" ");
         }else if(args[1] == "remove"){
-            var index = this.challenges.indexOf(args[3])
-            if(index > -1){this.challenges.splice(index, 1);}
+            this.rtm.sendMessage(this.removeChallenge(args[3]), message.channel);
         }else if(args[1] == "possible"){
             this.rtm.sendMessage(String(Object.keys(this.challenges).join(", ")), message.channel);
+        }else if(args[1] == "describe"){
+            if(args.length < 3){
+                this.rtm.sendMessage("Sorry, but the incorrect amount of parameters were provided for the 'describe' command. Correct usage is '@challengebot describe [challenge name]'", message.channel);
+                return;
+            }
+            this.rtm.sendMessage(this.describeChallenge(args[2]), message.channel);
+        }else if(args[1] == "current"){
+            if(this.lastTimeChallenging == null || this.lastTimeChallenging < new Date(getYear(), getMonth(), getDate(), 7, 30)){
+                this.lastTimeChallenging = new Date(getYear(), getMonth(), getDate(), getHours(), getMinutes());
+                var oldChallenge = this.currentChallenge;
+                do{
+                    this.currentChallenge = Object.keys(this.challenges)[Math.floor(Math.random() * this.challenges.length)];
+                }while(this.currentChallenge = oldChallenge);
+                this.removeChallenge(oldChallenge);
+            }
+            this.rtm.sendMessage(this.describeChallenge(this.currentChallenge), message.channel);
         }else{
-            this.rtm.sendMessage("To use this bot, type out '@challengebot' and then either 'add', 'remove', 'possible', 'describe', or 'current'.", message.channel);
+            this.rtm.sendMessage("Command not understood; use this bot, type out '@challengebot' and then either 'add', 'remove', 'possible', 'describe', or 'current'.", message.channel);
         }
+    }
+
+    function removeChallenge(challengeName){
+        if(challengeName == this.currentChallenge){
+            return "The challenge can't be deleted because it is the current challenge.";
+        }
+        delete this.challenges[challengeName];
+        return "The challenge was successfully deleted."
+    }
+
+    function describeChallenge(challengeName){
+        return challengeName + ":\n" + this.challenges[challengeName];
     }
 
 }
