be39dff838bafb81ca4a91e897fd564fa3a11693
Saurabh Totey <SaurabhTotey@gmail.com>
2017-07-18 11:30:26 -0600

    Deleted unecessary editing block

--- index.js
+++ index.js
@@ -3,11 +3,8 @@
 //Bots only actually get constructed if not editing
 //This makes sure the slack rate limit never comes into play
 'use strict';
-var editing = true;
 const ChallengeBot = require("./challengebot.js");
-if(!editing){
-    let challengebot = new ChallengeBot(process.env.challengeBotToken);
-}
+let challengebot = new ChallengeBot(process.env.challengeBotToken);
 
 //Below makes a webpage so that a timer can keep pinging the website
 //Prevents the script from going to sleep
